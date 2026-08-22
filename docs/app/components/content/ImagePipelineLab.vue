d347114f17882fc32e7a4ab4ec7277a357372ea4
Mat4m0 <matthias@lupinum.com>
2026-08-22 11:13:59 +0200

    fix(docs): render primitive image examples

--- docs/app/components/content/ImagePipelineLab.vue
+++ docs/app/components/content/ImagePipelineLab.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ImageAdapter } from '@lupinum/nuxt-photo/app'
+import { PhotoImage, type ImageAdapter } from '@lupinum/nuxt-photo/app'
 import { computed, nextTick, onMounted, ref, watch } from 'vue'
 import { demoPhotos } from '~/composables/demoPhotos'
 
